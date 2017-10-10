53dbd9c24b71170983529f5d55b6fa7b971d1b8b
Viktor Perdomo <vperdomo@orangetheoryfitness.com>
2017-10-10 16:02:46 -0400

    ES6 for js file

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 function loadData() {
 
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
+    const $body = $('body');
+    const $wikiElem = $('#wikipedia-links');
+    const $nytHeaderElem = $('#nytimes-header');
+    const $nytElem = $('#nytimes-articles');
+    const $greeting = $('#greeting');
 
     // clear out old data before new request
     $wikiElem.text("");
@@ -12,48 +12,47 @@ function loadData() {
 
     // load streetview
 
-    var $street = $("#street").val();
-    var $cityState = $("#cityState").val();
-    var $city = $cityState.split(',')[0];
-    var address = $street + ', ' + $cityState;
+    const $street = $("#street").val();
+    const $cityState = $("#cityState").val();
+    const $city = $cityState.split(',')[0];
+    const address = `${$street}, ${$cityState}`;
 
-    $greeting.text('So, you want to live at '+ address +'?');
-    $nytHeaderElem.text("New York Times Articles About "+ $cityState);
+    $greeting.text(`So, you want to live at ${address}?`);
+    $nytHeaderElem.text(`New York Times Articles About ${$cityState}`);
 
-    var streetviewUrl = '<img class="bgimg" src="https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' +
-           address + '">';
+    const streetviewUrl = `<img class="bgimg" src="https://maps.googleapis.com/maps/api/streetview?size=600x400&location=${address}">`;
 
     $body.append(streetviewUrl);
-    var apiKey = 'c1a798e2cfd9457a8d50decad1fa7774';
+    const apiKey = 'c1a798e2cfd9457a8d50decad1fa7774';
 
-    $.getJSON("http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+$city+"&sort=newest&api-key="+apiKey,
-        function( data ) {
-            var docs = data.response.docs;
-            var articles = [];
+    $.getJSON(`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${$city}&sort=newest&api-key=${apiKey}`,
+        data => {
+            const docs = data.response.docs;
+            const articles = [];
 
-            $.each(docs, function(index, value){
-                 articles.push('<li class="article"><a href="'+value.web_url+'">'+value.headline.main+'</a><p>' + value.snippet + '</p>');
+            $.each(docs, (index, value) => {
+                 articles.push(`<li class="article"><a href="${value.web_url}">${value.headline.main}</a><p>${value.snippet}</p>`);
             });
 
             $(articles.join( " " )).appendTo( $("#nytimes-articles") );
-        }).error(function() {
+        }).error(() => {
             $nytHeaderElem.text("New York Times Articles Could Not Be Loaded");
         }
     );
 
-    var wikiRequestTimeout = setTimeout(function(){
+    const wikiRequestTimeout = setTimeout(() => {
         $wikiElem.text("failed to get wikipedia resources");
     }, 8000);
 
-    $.ajax('http://en.wikipedia.org/w/api.php?action=opensearch&search='+$city+'&format=json&callback=wikiCallBack', {
+    $.ajax(`http://en.wikipedia.org/w/api.php?action=opensearch&search=${$city}&format=json&callback=wikiCallBack`, {
         dataType: "jsonp",
-        success: function(response) {
+        success(response) {
             console.log(response);
-            var articleTitles = response[1];
-            var articleLinks = response[3];
-            var numberOfArticles = articleLinks.length;
-            for(var i = 0; i < numberOfArticles; i++) {
-                $wikiElem.append('<li><a href="'+articleLinks[i]+'">'+articleTitles[i]+'</a></li>');
+            const articleTitles = response[1];
+            const articleLinks = response[3];
+            const numberOfArticles = articleLinks.length;
+            for(let i = 0; i < numberOfArticles; i++) {
+                $wikiElem.append(`<li><a href="${articleLinks[i]}">${articleTitles[i]}</a></li>`);
             }
 
             clearTimeout(wikiRequestTimeout);
